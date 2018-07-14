5b2bff8c13fa3a7b1f71a255c397bf861bc8194e
Oleg <oleg.golosov@gmail.com>
2018-07-14 07:52:22 +0200

    default arguments

--- RunUnigenQA.C
+++ RunUnigenQA.C
@@ -24,11 +24,17 @@ void RunUnigenQA (TString filePath = "/home/ogolosov/Desktop/analysis/mc/root/dc
 
 # ifndef __CINT__
 int main (int argc, char **argv) {
-		TString in = argv [1];
-		TString out = argv [2];
+		TString in;
+		TString out;
 		
-		in = "/home/ogolosov/Desktop/analysis/mc/root/dcmqgsm_12.root";
-		out = "/home/ogolosov/Desktop/analysis/mc/qa/qa_botvina_T0_12agev.root";
+		cout << argc << endl;
+		if (argc < 2) in = "*.root";
+		else in = argv [1];
+		if (argc < 3) out = "UnigenQA.root";
+		else out = argv [2];
+		
+		cout << "Input:" << in << endl;
+		cout << "Output:" << out << endl;
 		
     RunUnigenQA (in, out);
     return 0;
